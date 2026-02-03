3ea964587d4f95ce3c7247a96dc5ffb44fce4d98
nithiyashreek2004-design <nithiyashreek2004@gmail.com>
2026-02-03 14:44:57 +0530

    update

--- Day34.React function/src/App.jsx
+++ Day34.React function/src/App.jsx
@@ -1,13 +1,13 @@
-{/* Define a React component
+/* Define a React component
        A React component is just a function that shows something on the screen.
-       real-life example:TV remote = same button can be used many times. */}
+       real-life example:TV remote = same button can be used many times. */
 
-{/* Props explanation:
+/* Props explanation:
           - Props are arguments passed to components (like function parameters)
           - Props are read-only because React enforces unidirectional data flow
           - Modifying props directly would cause unpredictable behavior and bugs
           - Use state (useState) if you need to update values instead
-      */}
+      */
 //  Explain what state is   
     // State is data that can change over time
     // EXAMPLE :Likes = Number
@@ -62,16 +62,29 @@ function ToggleText() {
 /* Main App */
 function App() {
   const [count, setCount] = useState(0);
+  const [likes, setLikes] = useState(0);
+  const [isLightOn, setIsLightOn] = useState(false);
 
   return (
-    <div style={{ textAlign: "center" }}>
+       <div style={{
+      backgroundColor: isLightOn ? "#ffffff" : "#1e293b",color: isLightOn ? "#000000" : "#ffffff",
+      minHeight: "100vh",padding: "20px",textAlign: "center"}}>
       <h1>React Working ✅</h1>
 
       <button onClick={() => setCount(count + 1)}>count is {count}</button>
       <button onClick={() => setCount(0)}>Reset</button>
 
       <Greeting name="Shree Nithya.k" age={20} />
-
+        <div>
+        <h2>Like Feature</h2>
+        <button onClick={() => setLikes(likes + 1)}>👍 Like {likes}</button>
+      </div>
+        <div>
+        <h2>Light Switch</h2>
+        <button onClick={() => setIsLightOn(!isLightOn)}>
+          {isLightOn ? "Turn OFF Light" : "Turn ON Light"}
+        </button>
+      </div>
       <Card>
         <h2>Profile Card</h2>
         <h3>Tech Learner</h3>
@@ -91,29 +104,7 @@ function App() {
   );
 }
 
+
 export default App;
 
-
-
-
-// Define a React component
-// Create a functional component that shows your name
-// Pass age as a prop and display it
-// Identify props and children in a given code
-// Explain why props are read-only
-
-// Profile card using props (name, role)
-// Product list component with price props
-// Card component using children
-// Blog post layout using nested components
-// Simple dashboard layout using reusable components
-
-// Explain what state is
-// Create a counter using useState
-// Handle button click event
-// Capture input value using state
-// Toggle text visibility
-
-//Counter app with reset button
-//Light ON/OFF switch
 
