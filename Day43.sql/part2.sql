9661f1611008a0d4a234a282df60db3a7b1362a0
nithiyashreek2004-design <nithiyashreek2004@gmail.com>
2026-02-09 16:30:53 +0530

    update

---
+++ Day43.sql/part2.sql
@@ -0,0 +1,50 @@
+-- Draw a Database Table Structure (what to draw on paper)
+-- Table Name: STUDENTS
+-- ---------------------------------
+-- | student_id | name  | age | city |
+-- ---------------------------------
+-- | 1          | Ravi  | 20  | Chennai |
+-- | 2          | Meena | 22  | Mumbai  |
+-- ---------------------------------
+
+
+
+-- Databases Used in Daily Life
+
+-- Place	          What database stores
+-- Bank app	        Account details, transactions
+-- Instagram       	Users, posts, likes
+-- Amazon	            Products, orders, customers
+-- Hospital        	Patient records
+-- College         	Student marks
+-- WhatsApp        	Messages, contacts
+
+
+-- 3. SQL Workflow (Step by Step)
+--  User performs action (login, buy product)
+--  Application sends request to server
+--  Server runs SQL query
+--  Database processes query
+--  Database sends result back
+--  Server sends response to user
+
+
+-- File System vs Database
+
+-- File System	                Database
+-- Stores data in files	    Stores data in tables
+-- Hard to search          	Easy using SQL
+-- No relationships        	Supports relationships
+-- Low security	            High security
+-- No data rules           	Constraints & validation
+-- Slow for big data	        Fast & optimized
+
+
+-- Database Concept in 1 Minute
+
+-- “A database is an organized collection of data
+-- stored in tables. Each table has rows and columns.
+-- We use DBMS software like MySQL to manage databases.
+-- SQL is the language used to insert, update, delete, and retrieve data.
+-- Databases are used in banks, social media, shopping apps, hospitals, 
+-- and colleges to store and manage large amounts of information securely and efficiently.”
