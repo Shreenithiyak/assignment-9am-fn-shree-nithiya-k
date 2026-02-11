e8172ce4e2881a00ab2b3b2408c88232d65a6f6d
nithiyashreek2004-design <nithiyashreek2004@gmail.com>
2026-02-11 15:27:14 +0530

    update

--- Day44.sql/part2.sql
+++ Day44.sql/part2.sql
@@ -24,3 +24,34 @@
 -- INSERT INTO course (course_id, course_name, duration, fee)
 -- VALUES (1, 'React Basics', 3, 5000);
 
+-- create table enrollement(
+-- enroll_id INT PRIMARY KEY,
+-- course_id INT,
+-- student_id INT,
+-- FOREIGN KEY (student_id) REFERENCES student(student_id),
+-- FOREIGN KEY (course_id) REFERENCES course(course_id)
+-- );
+
+-- CREATE TABLE marks (
+--     mark_id INT PRIMARY KEY,
+--     student_id INT,
+--     subject VARCHAR(50),
+--     score INT CHECK (score BETWEEN 0 AND 100),
+--     FOREIGN KEY (student_id) REFERENCES student(student_id)
+-- );
+
+-- CREATE TABLE employees (
+--     emp_id INT PRIMARY KEY,
+--     name VARCHAR(100) NOT NULL,
+--     email VARCHAR(100) UNIQUE,
+--     salary INT CHECK (salary > 0),
+--     dept_id INT
+-- );
+
+-- CREATE TABLE departments (
+--     dept_id INT PRIMARY KEY,
+--     dept_name VARCHAR(100) NOT NULL UNIQUE
+-- );
+-- ALTER TABLE employees
+-- ADD FOREIGN KEY (dept_id) REFERENCES departments(dept_id);
+
