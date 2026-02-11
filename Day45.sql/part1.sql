179d27db06cd7253b888e3ec0072487f10d2fc76
nithiyashreek2004-design <nithiyashreek2004@gmail.com>
2026-02-11 16:54:28 +0530

    update

---
+++ Day45.sql/part1.sql
@@ -0,0 +1,16 @@
+-- ALTER TABLE employees
+-- ADD department varchar(50);
+-- ALTER TABLE employees
+-- RENAME column name to emp_name;
+-- INSERT INTO employees(emp_id,emp_name,email,salary,department)
+-- VALUES( 2, "shree","me@gmail.com",8000000,"IT"),
+-- ( 3, "krish","dv@gmail.com",8000000,"NON IT"),
+-- ( 4, "adhvik","ug@gmail.com",8000000,"IT"),
+-- ( 5, "kayal","man@gmail.com",8000000,"NON IT");
+
+-- UPDATE employees
+-- SET salary = 45000
+-- WHERE emp_id = 1;
+
+-- DELETE FROM employees
+-- WHERE emp_id = 1;
